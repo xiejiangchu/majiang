97a7f405616982cdf4795c749b4ac97237474c4e
xiejiangchu <1235116@tongji.edu.cn>
2017-08-22 11:05:34 +0800

    启动脚本

--- server/project/distribution/src/startup.sh
+++ server/project/distribution/src/startup.sh
@@ -5,5 +5,5 @@ nohup java   -server  -verbose:gc   -cp ./client majiang.client.ClientMain > log
 nohup java   -server  -verbose:gc   -cp ./manager majiang.client.ManagerMain > log/ManagerMain.log 2>&1 &
 nohup java   -server  -verbose:gc   -cp ./boss game.boss.BossMain > log/BossMain.log 2>&1 &
 nohup java   -server  -verbose:gc   -cp ./scene game.scene.SceneMain > log/SceneMain.log 2>&1 &
-java   -server  -verbose:gc   -cp ./gateway game.gateway.GatewayMain > log/GatewayMain.log >&1 2>&1
+nohup java   -server  -verbose:gc   -cp ./gateway game.gateway.GatewayMain > log/GatewayMain.log 2>&1 &
 
